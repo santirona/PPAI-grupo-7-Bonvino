9e2d1610393fed3d17c08ad631c5423426cf2a9d
Mateo <mateo.j.ramallo@gmail.com>
2024-05-23 20:18:45 -0300

    Segunda pagina para pedir datos, y se llega hasta validarFecha

--- DiseñoEstatico/js/script.js
+++ DiseñoEstatico/js/script.js
@@ -14,3 +14,20 @@ function boton3() {
 function boton4() {
     alert("Funcionalidad del botón 4");
 }
+
+function botonBuscar(){
+    /* alert("Funcionalidad del botón buscar"); */
+    //Tomar los datos del formulario
+    var fechaDesde = document.getElementById("fechaDesde").value;
+    var fechaHasta = document.getElementById("fechaHasta").value;
+    //Mostrarlos por consola del navegador
+    console.log("Fecha desde: " + fechaDesde);
+    console.log("Fecha hasta: " + fechaHasta);
+    //Ahora tenemos que validar que las fechas sean validas, son validas si la fechaHasta es mayor a la fechaDesde y ambas estan en formato de fecha.
+    if (Date.parse(fechaDesde) > Date.parse(fechaHasta)){
+        alert("La fecha desde no puede ser mayor que la fecha hasta.");
+        //Si estamos en este flujo se valido el periodo con exito
+        
+    }
+
+}
