dbbffdc6d7181a564147839479dbbe17e5ea5ef4
Emiliano Diaz <95255@sistemas.frc.utn.edu.ar>
2024-05-26 15:53:41 -0300

    Creacion funcion Provincia

---
+++ Backend/Clases/clase_provincia.py
@@ -0,0 +1,15 @@
+from clase_pais import Pais
+
+class Provincia:
+    def __init__(self, nombre, pais):
+        self.nombre = nombre
+        self.pais = pais
+
+    def contarRegiones(self):
+        pass
+
+    def mostrarRegiones(self):
+        pass
+
+    def obtenerPais(self):
+        return self.pais.nombre 
