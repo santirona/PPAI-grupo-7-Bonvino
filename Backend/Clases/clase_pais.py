5eb849095da9ac1b109612e348ba76d9fdc2bf8f
fededlV <fededelavega2@gmail.com>
2024-05-26 16:30:25 -0300

    Terminando provincia y pais

--- Backend/Clases/clase_pais.py
+++ Backend/Clases/clase_pais.py
@@ -1,13 +1,13 @@
-from clase_bodega import Bodega
+from clase_provincia import Provincia
 
 class Pais:
-    def __init__(self, nombre):
+    def __init__(self, nombre, provinciaNombre):
         self.nombre = nombre
+        self.provincia = Provincia(provinciaNombre)
     
     """ def contarBodegas(self):
         return len(self.) """ 
 
     def getNombre(self):
         return self.nombre
-    
     
