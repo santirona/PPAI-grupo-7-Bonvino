5a9fb1fe37207d493c93acca4a82b6cf2b284d71
Santiago Orona <95342@sistemas.frc.utn.edu.ar>
2024-05-26 16:01:08 -0300

    agrego cambios en vino y varietal

--- Backend/Clases/clase_vino.py
+++ Backend/Clases/clase_vino.py
@@ -1,13 +1,34 @@
 class Vino: 
-    def __init__(self, id, nombre, bodega, region_vitivinicola, provincia, pais, varietal, precio):
+    def __init__(self, id, nombre, bodega, region_vitivinicola, provincia, pais, varietales, precio, reseñas):
         self.id = id
         self.nombre = nombre
         self.bodega = bodega
         self.region_vitivinicola = region_vitivinicola
         self.provincia = provincia
         self.pais = pais
-        self.varietal = varietal
+        self.varietales = varietales
         self.precio = precio
+        self.reseñas = reseñas
+    
+    def getVarietales(self):
+        return [varietal.getDescripcion() for varietal in self.varietales]
+    
+    ###
+    # ejemplo
+        varietal1 = Varietal.new("Cabernet Sauvignon", 85)
+        varietal2 = Varietal.new("Merlot", 15)
+        vino = Vino(1, "Gran Reserva", "Bodega XYZ", "Mendoza", "Mendoza", "Argentina", [varietal1, varietal2], 25.50)
+    ###
+    
     
     def obtener_info(self):
-        return f"{self.nombre}, {self.varietal}, {self.bodega.nombre}, {self.region_vitivinicola.nombre}, {self.provincia.nombre}, {self.pais.nombre}, {self.precio}"
+        return f"{self.nombre}, {self.varietales}, {self.bodega.nombre}, {self.region_vitivinicola.nombre}, {self.provincia.nombre}, {self.pais.nombre}, {self.precio}"
+    
+    def tenes_reseñas_de_tipo_en_periodo(self, tipo, desde, hasta):
+        pass
+    
+    def getNombre(self):
+        return self.nombre
+    
+    def getPrecio(self):
+        return self.precio
